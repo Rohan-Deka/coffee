57f22037d95210d29e82154950935df2053fbf47
Ron <rohandeka614@gmail.com>
2021-05-27 13:32:25 +0530

    Initial commit

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,20 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on =True
+while is_on:
+    options = coffee_maker.get_items()
+    choice = input(f"What would you like to have {options}: ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
